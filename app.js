07e40562856f5a07f54da1ad3745468e0d6563eb
LusCard <lucascardenas@outlook.es>
2024-08-06 14:48:21 -0300

    Server is up

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res)=>{
+   res.send("hol")})
+   
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}`)})

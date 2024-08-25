1d40e4a2895920bbb05c96b9819003e1b8b0ef26
Estudiante <luquiscardenas@gmail.com>
2024-08-25 17:36:24 -0300

    Nothing important

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import morgan, { format } from "morgan";
+import morgan from "morgan";
 import { connectDB } from "./src/config/database.js";
 import { createTable } from "./src/controllers/controllers.js";
 import { router } from "./src/routes/routes.js";
